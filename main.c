62bf97ec52e2bfe67f64a3fc8e1e388707c5701a
Kota Kato <me@kat0h.com>
2022-05-18 23:54:28 +0900

    Tokenize

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+void prompt() {
   printf("$ ");
+}
+
+void read_line(char* line, int size) {
+  fgets(line, size, stdin);
+}
+
+int main(int argc, char *argv[]) {
+  prompt();
 
   char line[1000];
-  fgets(line, sizeof(line), stdin);
-  printf("%s", line);
+  read_line(line, sizeof(line));
+
+  int c = 0;
+  while(line[c] != '\0') {
+    char chr = line[c];
+    if (chr == ' ') {
+      printf("\n");
+    } else {
+      printf("%c", chr);
+    }
+    c++;
+  }
+
   return 0;
 }
